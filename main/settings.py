c6670bae80761f2f8901c08e21d0cb486616db09
Alex <alexthomas1912@gmail.com>
2026-01-25 18:05:37 +0000

    Create custom secret key using environment variables to secure databases

--- main/settings.py
+++ main/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!klcrnxij4vgx3n07mvu!xwo7@%v!d+l6=y1hh)0^c%8=t2r4w'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
