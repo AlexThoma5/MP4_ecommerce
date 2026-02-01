9af30501ed340bf3617d0576fdb1723e50a36800
Alex <alexthomas1912@gmail.com>
2026-02-01 17:18:16 +0000

    Add service model with fields, apply Meta class for ordering

--- services/models.py
+++ services/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+SERVICE_TYPE_CHOICES = [
+    (1, 'Initial Appointment'),
+    (2, 'Follow-up Appointment'),
+    (3, 'Package Deal'),
+    (4, 'Treatment'),
+]
+
+
+class Service(models.Model):
+    """
+    Stores a single service.
+    """
+    service_type = models.PositiveSmallIntegerField(
+        choices=SERVICE_TYPE_CHOICES)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    duration = models.PositiveIntegerField()
+    session_count = models.PositiveIntegerField(default=1)
+    image = models.ImageField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['service_type']
+
+    def __str__(self):
+        return self.name
