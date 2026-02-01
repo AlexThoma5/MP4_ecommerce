d675185751146066d297966bb9dc597d6a37417d
Alex <alexthomas1912@gmail.com>
2026-02-01 17:19:55 +0000

    Add Service model to admin panel with custom list display and ordering

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Service
 
 # Register your models here.
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'service_type',
+        'name',
+        'duration',
+        'price',
+        'session_count'
+    )
+
+    ordering = ['service_type']
