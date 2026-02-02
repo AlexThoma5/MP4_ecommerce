9199d22af9001fd760e69451d9a35800a991d5ca
Alex <alexthomas1912@gmail.com>
2026-02-02 19:53:16 +0000

    Add all_services view with services grouped by service type and wire up urls

--- services/urls.py
+++ services/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test, name='test_view'),
+    path('', views.all_services, name='services'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,7 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Service
 
 
 # Create your views here.
-def test(request):
-    return HttpResponse("Test view for services app")
+def all_services(request):
+    """
+    Display services page with all :model:`services.Service` objects,
+    grouped by service type.
+
+    **Context**
+
+    ``services``
+        All instances of :model:`services.Service`.
+    ``initial_services``
+        All initial appointment services.
+    ``follow_up_services``
+        All follow-up appointment services.
+    ``package_services``
+        All package deal services.
+    ``treatment_services``
+        All treatment services.
+
+    **Template:**
+
+    :template:`services/services.html`
+    """
+
+    services = Service.objects.all()
+
+    initial_services = services.filter(service_type=1)
+    follow_up_services = services.filter(service_type=2)
+    package_services = services.filter(service_type=3)
+    treatment_services = services.filter(service_type=4)
+
+    context = {
+        "services": services,
+        "initial_services": initial_services,
+        "follow_up_services": follow_up_services,
+        "package_services": package_services,
+        "treatment_services": treatment_services,
+    }
+
+    return render(request, 'services/services.html', context)
