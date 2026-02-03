bf95c87c7c08494621efe7a7fd3097aa87d38cdf
Alex <alexthomas1912@gmail.com>
2026-02-03 07:13:24 +0000

    Add service_detail view, URL, and basic template

--- services/views.py
+++ services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Service
 
 
@@ -42,3 +42,26 @@ def all_services(request):
     }
 
     return render(request, 'services/services.html', context)
+
+
+def service_detail(request, service_id):
+    """
+    Display an individual :model:`services.Service`.
+
+    **Context**
+
+    ``service``
+        An instance of :model:`services.Service`.
+
+    **Template:**
+
+    :template:`services/service_detail.html`
+    """
+
+    service = get_object_or_404(Service, pk=service_id)
+
+    context = {
+        'service': service,
+    }
+
+    return render(request, 'services/service_detail.html', context)
