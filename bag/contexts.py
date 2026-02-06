f9374e27f706e4df720d0d8faa477bb5e6ce5814
Alex <alexthomas1912@gmail.com>
2026-02-06 04:51:52 +0000

    Add add_to_bag view, url and update context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,8 +1,24 @@
-def bag_contents(request):
+from django.shortcuts import get_object_or_404
+from services.models import Service
 
+
+def bag_contents(request):
+    """
+    Returns context data for the shopping bag containing services.
+    """
     bag_items = []
     total = 0
     service_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id in bag.keys():
+        service = get_object_or_404(Service, pk=item_id)
+        total += service.price
+        service_count += 1
+        bag_items.append({
+            'item_id': item_id,
+            'service': service,
+        })
 
     grand_total = total
 
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,19 @@ from django.shortcuts import render
 def view_bag(request):
     """ A view that renders the bag contents page """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a single service to the shopping bag """
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+    quantity = 1
+
+    if item_id not in bag:
+        bag[item_id] = quantity
+        # message for successful addition to bag
+        # else item already exists message
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
