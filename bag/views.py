01e98544b93e9121ec9391cb98b2014f2ecd246c
Alex <alexthomas1912@gmail.com>
2026-02-08 17:46:44 +0000

    Add Django messages to bag views

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from services.models import Service
 # Create your views here.
 
 
@@ -12,14 +12,16 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a single service to the shopping bag """
 
+    service = get_object_or_404(Service, pk=item_id)
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
     quantity = 1
 
     if item_id not in bag:
         bag[item_id] = quantity
-        # message for successful addition to bag
-        # else item already exists message
+        messages.success(request, f'<strong>{service.name}</strong><br>Added to your bag')
+    else:
+        messages.info(request, f'<strong>{service.name}</strong><br>Is already in your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -28,9 +30,11 @@ def add_to_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """ Remove the service item from the shopping bag """
 
+    service = get_object_or_404(Service, pk=item_id)
     bag = request.session.get('bag', {})
 
     bag.pop(item_id, None)
+    messages.success(request, f'<strong>{service.name}</strong><br>Has been removed from bag')
 
     request.session['bag'] = bag
 
