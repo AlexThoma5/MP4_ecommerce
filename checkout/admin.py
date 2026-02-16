9976e9b88e0659963f4a1036d0cbf92ba90595aa
Alex <alexthomas1912@gmail.com>
2026-02-16 08:47:02 +0000

    Add Discount model to admin and integrate with OrderAdmin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem
+from .models import Order, OrderLineItem, Discount
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
@@ -12,14 +12,25 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
     readonly_fields = ('order_number', 'date',
                        'order_total', 'grand_total',
-                       'original_bag', 'stripe_pid')
+                       'original_bag', 'stripe_pid',
+                       'discount', 'discount_amount',)
 
     fields = ('order_number', 'date', 'full_name',
-              'email', 'phone_number', 'order_total',
-              'grand_total', 'original_bag', 'stripe_pid')
+              'email', 'phone_number', 'discount',
+              'order_total', 'discount_amount', 'grand_total',
+              'original_bag', 'stripe_pid',)
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total',
                     'grand_total',)
 
     ordering = ('-date',)
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('code', 'percentage', 'active', 'created_on')
+    list_filter = ('active',)
+    search_fields = ('code',)
+    fields = ('code', 'percentage', 'active', 'created_on')
+    readonly_fields = ('created_on',)
