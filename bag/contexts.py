8916b1141345f4b7a45dc48b65dedcb30f2b23a1
Alex <alexthomas1912@gmail.com>
2026-02-16 08:48:22 +0000

    Add discount calculation to bag context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
+from decimal import Decimal
 from django.shortcuts import get_object_or_404
 from services.models import Service
+from checkout.models import Discount
 
 
 def bag_contents(request):
@@ -9,6 +11,7 @@ def bag_contents(request):
     bag_items = []
     total = 0
     service_count = 0
+
     bag = request.session.get('bag', {})
 
     for item_id in bag.keys():
@@ -20,12 +23,29 @@ def bag_contents(request):
             'service': service,
         })
 
-    grand_total = total
+    # Initialise discount variables for context
+    discount = None
+    discount_amount = 0
+    discount_id = request.session.get('discount_id')
+
+    if discount_id and total > 0:
+        try:
+            discount = Discount.objects.get(pk=discount_id, active=True)
+            discount_amount = round(
+                total * Decimal(discount.percentage / 100), 2)
+        except Discount.DoesNotExist:
+            request.session.pop('discount_id', None)
+            discount = None
+            discount_amount = 0
+
+    grand_total = total - discount_amount
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'service_count': service_count,
+        'discount': discount,
+        'discount_amount': discount_amount,
         'grand_total': grand_total,
     }
 
