d95b31edffc7dc58dc8d5fabcef612df96d8d24d
Alex <alexthomas1912@gmail.com>
2026-02-19 15:53:40 +0000

    Add post form handling to profile view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
+from checkout.models import Order
 
-# Create your views here.
+
+@login_required
+def profile(request):
+    """ Display the user's profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your details have been updated!')
+        else:
+            messages.error(
+                request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+
+    orders = profile.orders.all()
+
+    context = {
+        'form': form,
+        'orders': orders,
+    }
+
+    return render(request, 'profiles/profile.html', context)
