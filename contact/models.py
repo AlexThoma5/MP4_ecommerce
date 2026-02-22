f9616db32ef5d8eac9ea2fe0c42ccb5dad178d6c
Alex <alexthomas1912@gmail.com>
2026-02-22 05:23:55 +0000

    Add ContactMessage model to contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+SUBJECT_CHOICES = [
+    (1, 'General Enquiry'),
+    (2, 'Treatment Information'),
+    (3, 'Pricing Question'),
+    (4, 'Existing Client Query'),
+    (5, 'Other'),
+]
+
+
+class ContactMessage(models.Model):
+    """
+    Stores a single contact messsage.
+    """
+    full_name = models.CharField(max_length=64)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    subject = models.PositiveSmallIntegerField(
+        choices=SUBJECT_CHOICES)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-date']
+        verbose_name = 'Contact Message'
+        verbose_name_plural = 'Contact Messages'
+
+    def __str__(self):
+        return f'Contact message from {self.full_name}'
