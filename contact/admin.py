16693a36741aede655b5c2139923ecba8773a686
Alex <alexthomas1912@gmail.com>
2026-02-22 05:26:24 +0000

    Add ContactMessage model to admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'full_name', 'email', 'date', 'read')
+    list_filter = ('date', 'read',)
+    search_fields = ('full_name', 'email')
+    fields = ('full_name', 'subject', 'email',
+              'phone_number', 'message', 'read',
+              'date')
+    readonly_fields = ('date',)
