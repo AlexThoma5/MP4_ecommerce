7274781a578034b325abe79716f810e515227868
Alex <alexthomas1912@gmail.com>
2026-02-25 15:50:36 +0000

    Fix edit service functionality bug

--- services/views.py
+++ services/views.py
@@ -116,7 +116,7 @@ def edit_service(request, service_id):
     service = get_object_or_404(Service, pk=service_id)
 
     if request.method == "POST":
-        form = ServiceForm(request.POST, request.FILES, instance=service)
+        form = ServiceForm(request.POST, request.FILES, instance=service, prefix="edit")
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully updated service!')
