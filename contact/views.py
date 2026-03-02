ff95265b41db237892e86c59551d63907a2c9711
Alex <alexthomas1912@gmail.com>
2026-03-02 03:31:41 +0000

    Fix missing indentation on contact view for code validation, improve docstring

--- contact/views.py
+++ contact/views.py
@@ -10,10 +10,14 @@ from .models import CompanyDetails
 def contact_view(request):
     """
     Renders the contact page with the instance
-    of :form:`contact.ContactForm`.
+    of :form:`contact.ContactForm` and the latest
+    instance of :model:`contact.CompanyDetails`.
     **Context**
     ``contact_form``
         An instance of :form:`contact.ContactForm`.
+    ``company_details``
+        The most recently updated instance of
+        :model:`contact.CompanyDetails`.
     **Template**
         :template:`contact/contact.html`
     """
@@ -23,16 +27,18 @@ def contact_view(request):
             instance = contact_form.save()
             send_mail(
                 subject='Thanks for contacting us!',
-                message=f"Hi {instance.full_name},\n\n"
-                        "Thanks for reaching out. We have received your message "
-                        "and will respond within 3 days.",
+                message=f'Hi {instance.full_name},\n\n'
+                        f'Thanks for reaching out. We have received your '
+                        f'message and will respond within 3 days.',
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 recipient_list=[instance.email],
                 fail_silently=False,
             )
-            messages.success(request,
-                             "Your message has been received!"
-                             " We will aim to respond within 3 days")
+            messages.success(
+                request,
+                "Your message has been received! "
+                "We will aim to respond within 3 days"
+            )
             return redirect('contact')
         else:
             messages.error(
